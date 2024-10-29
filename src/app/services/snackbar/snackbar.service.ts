0111fa37d0daa3594be42f114c1a5e0e18b9c786
Rashmi K <Rashmi K>
2024-10-29 12:56:30 +0530

    snackbar service

---
+++ src/app/services/snackbar/snackbar.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SnackbarService {
+
+  constructor(private snackbar: MatSnackBar) { }
+
+
+  openSnack(message: string, actionText = 'OK', duration = 5000) {
+    this.snackbar.open(message, actionText, {
+      duration: duration
+    })
+  }
+
+}
