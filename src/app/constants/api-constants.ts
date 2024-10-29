43d17292eaa6cdc5e2d5d5778eb17f7d99d598e5
Rashmi K <Rashmi K>
2024-10-29 12:44:00 +0530

    add status and route constants for API usage

---
+++ src/app/constants/api-constants.ts
@@ -0,0 +1,8 @@
+// status constants
+export const STATUS_SUCCESS = 1
+export const STATUS_FAILED = -1
+export const STATUS_NO_RECORDS = -1
+
+// routes
+export const AUTHENTICATE = 'auth/authenticate'
+export const VALIDATE_EXTERNAL_SESSION = 'report/validate-external-session'
