f1c45b38628e261aecf6e43afd84ec2ebd96e96d
Rashmi K <Rashmi K>
2024-11-12 13:01:53 +0530

    router updates

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { routes as routeConstants } from '.././app/constants/routes';
 
-const routes: Routes = [];
+import { LayoutComponent } from './core/layout/layout.component';
+
+const { LOGIN, DASHBOARD, USER, ADD, EDIT } = routeConstants
+
+const routes: Routes = [
+  { path: '', redirectTo: `/${LOGIN}`, pathMatch: 'full' },
+  { path: LOGIN, loadChildren: () => import('./pages/login/login.module').then(m => m.LoginModule) },
+  {
+    path: '', component: LayoutComponent, children: [
+      { path: DASHBOARD, loadChildren: () => import('./pages/dashboard/dashboard.module').then(m => m.DashboardModule) },
+      { path: USER, loadChildren: () => import('./pages/user/user.module').then(m => m.UserModule) },
+    ]
+  }
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
